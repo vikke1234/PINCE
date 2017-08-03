350894fb1a7c3c780fb412147e7e58f23721f5e7
jakob <jakob@memeware.net>
2017-08-03 16:09:25 -0400

    Fix Travis

--- libPINCE/ElfUtils.py
+++ libPINCE/ElfUtils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2017 Jakob Kreuze <jakob@memeware.net>
+Copyright (C) 2016-2017 Jakob Kreuze <jakob@memeware.net>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
